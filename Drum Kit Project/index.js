d0177ab7962a3ae6bb64512d182fd90056b0180d
Umesh <brijbiharisingh1971@outlook.com>
2024-06-29 17:33:38 +0530

    Drum Kit Project Completed

---
+++ Drum Kit Project/index.js
@@ -0,0 +1,70 @@
+
+
+function handleClick(){
+   var buttonClicked = this.innerHTML;
+   playSound(buttonClicked);
+   animateButton(buttonClicked);
+}
+
+var arr = document.querySelectorAll(".drum");
+
+for(var i=0; i<arr.length; i++){
+    arr[i].addEventListener("click", handleClick);
+    
+}
+
+document.addEventListener("keydown", function(event){
+    playSound(event.key);
+    animateButton(event.key);
+});
+
+function playSound(buttonClicked){
+    switch(buttonClicked){
+        case "w":
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        
+        case "k":
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "l":
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+
+        default: console.log(buttonClicked);
+        
+    }
+}
+
+function animateButton(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
